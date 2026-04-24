117d97deebca8e3453c212bc30cf429b48620ccb
Ruoave <1037119039@qq.com>
2026-04-25 03:18:31 +0800

    UI初改 根本没用上控件，排布不好，功能也都还没有

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -26,7 +26,8 @@
 #include <QFileDialog>
 #include <QString>
 #include <QStandardPaths>
-#include "qtmaterialautocomplete.h"
+#include <QImage>
+#include <QPixmap>
 
 using namespace cv;
 using namespace std;
@@ -37,16 +38,106 @@ using namespace chrono;
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow),
-    darkSectioning(new DarkSectioning(ui))
+    darkSectioning(new DarkSectioning(ui)),
+    currentFrame(0),
+    isSyncMode(false)
 {
     ui->setupUi(this);
-
+    
+    // 初始化Material组件
+    // 1. 初始化开关控件
+    m_toggle = new QtMaterialToggle(this);
+    
+    // 2. 初始化复选框控件
+    m_checkbox = new QtMaterialCheckBox(this);
+    
+    // 3. 初始化文本输入框
+    m_textFields.append(new QtMaterialTextField(this));
+    m_textFields.append(new QtMaterialTextField(this));
+    m_textFields.append(new QtMaterialTextField(this));
+    m_textFields.append(new QtMaterialTextField(this));
+    m_textFields.append(new QtMaterialTextField(this));
+    m_textFields.append(new QtMaterialTextField(this));
+    m_textFields.append(new QtMaterialTextField(this));
+    m_textFields.append(new QtMaterialTextField(this));
+    m_textFields.append(new QtMaterialTextField(this));
+    m_textFields.append(new QtMaterialTextField(this));
+    m_textFields.append(new QtMaterialTextField(this));
+    
+    // 4. 初始化按钮
+    m_runButton = new QtMaterialRaisedButton("Run Dark Sectioning", this);
+    m_browseButton = new QtMaterialFlatButton("浏览", this);
+    m_browseOutputButton = new QtMaterialFlatButton("浏览", this);
+    
+    // 5. 初始化图标按钮
+    m_prevButton = new QtMaterialIconButton(QIcon(":/icons/prev.png"), this);
+    m_nextButton = new QtMaterialIconButton(QIcon(":/icons/next.png"), this);
+    m_prevButton2 = new QtMaterialIconButton(QIcon(":/icons/prev.png"), this);
+    m_nextButton2 = new QtMaterialIconButton(QIcon(":/icons/next.png"), this);
+    
+    // 6. 初始化进度条
+    m_progressBar = new QtMaterialProgress(this);
+    m_progressBar->setRange(0, 100);
+    
+    // 7. 初始化滑块
+    m_slider1 = new QtMaterialSlider(this);
+    m_slider1->setRange(0, 0);
+    m_slider2 = new QtMaterialSlider(this);
+    m_slider2->setRange(0, 0);
+    
+    // 8. 初始化同步按钮
+    m_syncButton = new QtMaterialRaisedButton("同步帧", this);
+    
+    // 信号槽绑定
+    // 帧切换按钮
+    connect(m_prevButton, &QtMaterialIconButton::clicked, this, &MainWindow::onFramePrev);
+    connect(m_nextButton, &QtMaterialIconButton::clicked, this, &MainWindow::onFrameNext);
+    connect(m_prevButton2, &QtMaterialIconButton::clicked, this, &MainWindow::onFramePrev2);
+    connect(m_nextButton2, &QtMaterialIconButton::clicked, this, &MainWindow::onFrameNext2);
+    
+    // 同步按钮
+    connect(m_syncButton, &QtMaterialRaisedButton::clicked, this, &MainWindow::onSyncFrames);
+    
+    // 滑块信号
+    connect(m_slider1, &QtMaterialSlider::valueChanged, [this](int value) {
+        currentFrame = value;
+        if (isSyncMode) {
+            m_slider2->setValue(value);
+        }
+        // 更新图像显示
+        updateImageDisplay();
+    });
+    
+    connect(m_slider2, &QtMaterialSlider::valueChanged, [this](int value) {
+        currentFrame = value;
+        if (isSyncMode) {
+            m_slider1->setValue(value);
+        }
+        // 更新图像显示
+        updateImageDisplay();
+    });
 }
 
 MainWindow::~MainWindow()
 {
     delete darkSectioning;
     delete ui;
+    
+    // 清理Material组件
+    delete m_toggle;
+    delete m_checkbox;
+    qDeleteAll(m_textFields);
+    delete m_runButton;
+    delete m_browseButton;
+    delete m_browseOutputButton;
+    delete m_prevButton;
+    delete m_nextButton;
+    delete m_prevButton2;
+    delete m_nextButton2;
+    delete m_progressBar;
+    delete m_slider1;
+    delete m_slider2;
+    delete m_syncButton;
 }
 
 void MainWindow::on_pushButton_clicked()
@@ -54,6 +145,21 @@ void MainWindow::on_pushButton_clicked()
     ui->textEdit->append("开始图像处理...");
     darkSectioning->process();
     ui->textEdit->append("图像处理完成!");
+    
+    // 处理完成后显示滑块，隐藏进度条
+    m_progressBar->hide();
+    m_slider1->show();
+    m_slider2->show();
+    
+    // 更新滑块范围
+    if (!imageStack.empty()) {
+        m_slider1->setRange(0, imageStack.size() - 1);
+        m_slider2->setRange(0, final_images.size() - 1);
+        m_slider1->setValue(0);
+        m_slider2->setValue(0);
+        currentFrame = 0;
+        updateImageDisplay();
+    }
 }
 
 void MainWindow::on_pushButton_browse_clicked()
@@ -115,6 +221,88 @@ void MainWindow::on_pushButton_browseOutput_clicked()
     }
 }
 
+// 项目自定义槽函数：处理帧切换
+void MainWindow::onFramePrev()
+{
+    if (currentFrame > 0) {
+        currentFrame--;
+        m_slider1->setValue(currentFrame);
+        if (isSyncMode) {
+            m_slider2->setValue(currentFrame);
+        }
+        updateImageDisplay();
+    }
+}
+
+void MainWindow::onFrameNext()
+{
+    if (currentFrame < imageStack.size() - 1) {
+        currentFrame++;
+        m_slider1->setValue(currentFrame);
+        if (isSyncMode) {
+            m_slider2->setValue(currentFrame);
+        }
+        updateImageDisplay();
+    }
+}
+
+void MainWindow::onFramePrev2()
+{
+    if (currentFrame > 0) {
+        currentFrame--;
+        m_slider2->setValue(currentFrame);
+        if (isSyncMode) {
+            m_slider1->setValue(currentFrame);
+        }
+        updateImageDisplay();
+    }
+}
+
+void MainWindow::onFrameNext2()
+{
+    if (currentFrame < final_images.size() - 1) {
+        currentFrame++;
+        m_slider2->setValue(currentFrame);
+        if (isSyncMode) {
+            m_slider1->setValue(currentFrame);
+        }
+        updateImageDisplay();
+    }
+}
+
+// 项目自定义槽函数：处理同步按钮点击
+void MainWindow::onSyncFrames()
+{
+    isSyncMode = !isSyncMode;
+    if (isSyncMode) {
+        // 同步处理前后图片的显示帧数
+        m_slider1->setValue(currentFrame);
+        m_slider2->setValue(currentFrame);
+        m_syncButton->setText("同步已开启");
+    } else {
+        m_syncButton->setText("同步帧");
+    }
+}
+
+// 辅助函数：更新图像显示
+void MainWindow::updateImageDisplay()
+{
+    if (currentFrame >= 0 && currentFrame < imageStack.size()) {
+        // 显示处理前图片
+        Mat img = imageStack[currentFrame];
+        QImage qimg(img.data, img.cols, img.rows, img.step, QImage::Format_RGB888);
+        QPixmap pixmap = QPixmap::fromImage(qimg);
+        ui->label_original->setPixmap(pixmap.scaled(ui->label_original->size(), Qt::KeepAspectRatio));
+    }
+    
+    if (currentFrame >= 0 && currentFrame < final_images.size()) {
+        // 显示处理后图片
+        Mat img = final_images[currentFrame];
+        QImage qimg(img.data, img.cols, img.rows, img.step, QImage::Format_RGB888);
+        QPixmap pixmap = QPixmap::fromImage(qimg);
+        ui->label_processed->setPixmap(pixmap.scaled(ui->label_processed->size(), Qt::KeepAspectRatio));
+    }
+}
 
 //
 
--- mainwindow.h
+++ mainwindow.h
@@ -13,6 +13,17 @@
 #include <ViewMat.h>
 #include <vector>
 
+// Material组件头文件引入
+#include "qtmaterialtoggle.h"
+#include "qtmaterialcheckbox.h"
+#include "qtmaterialtextfield.h"
+#include "qtmaterialraisedbutton.h"
+#include "qtmaterialflatbutton.h"
+#include "qtmaterialiconbutton.h"
+#include "qtmaterialprogress.h"
+#include "qtmaterialslider.h"
+#include "lib/qtmaterialtheme.h"
+
 namespace Ui {
 class MainWindow;
 }
@@ -26,13 +37,62 @@ public:
     ~MainWindow();
 
 private slots:
+    // Qt原生公共槽函数：响应界面按钮点击信号
+    // 信号源：ui->pushButton clicked信号
+    // 功能：运行Dark Sectioning处理
     void on_pushButton_clicked();
+    
+    // Qt原生公共槽函数：响应浏览按钮点击信号
+    // 信号源：ui->pushButton_browse clicked信号
+    // 功能：打开文件选择对话框选择输入图片路径
     void on_pushButton_browse_clicked();
+    
+    // Qt原生公共槽函数：响应浏览按钮点击信号
+    // 信号源：ui->pushButton_browseOutput clicked信号
+    // 功能：打开文件夹选择对话框选择输出目录
     void on_pushButton_browseOutput_clicked();
+    
+    // 项目自定义槽函数：处理帧切换
+    // 信号源：前后箭头按钮clicked()信号
+    // 功能：切换图像帧
+    void onFramePrev();
+    void onFrameNext();
+    void onFramePrev2();
+    void onFrameNext2();
+    
+    // 项目自定义槽函数：处理同步按钮点击
+    // 信号源：ui->pushButton_sync clicked信号
+    // 功能：同步处理前后图片的显示帧数
+    void onSyncFrames();
+    
+    // 辅助函数：更新图像显示
+    void updateImageDisplay();
 
 private:
     Ui::MainWindow *ui;
     DarkSectioning *darkSectioning;
+    
+    // Material组件声明
+    QtMaterialToggle *m_toggle;               // Material设计风格开关控件
+    QtMaterialCheckBox *m_checkbox;           // Material设计风格复选框控件
+    QList<QtMaterialTextField*> m_textFields; // Material设计风格文本输入框列表
+    QtMaterialRaisedButton *m_runButton;      // Material设计风格凸起按钮
+    QtMaterialFlatButton *m_browseButton;     // Material设计风格扁平按钮
+    QtMaterialFlatButton *m_browseOutputButton; // Material设计风格扁平按钮
+    QtMaterialIconButton *m_prevButton;       // Material设计风格图标按钮
+    QtMaterialIconButton *m_nextButton;       // Material设计风格图标按钮
+    QtMaterialIconButton *m_prevButton2;      // Material设计风格图标按钮
+    QtMaterialIconButton *m_nextButton2;      // Material设计风格图标按钮
+    QtMaterialProgress *m_progressBar;        // Material设计风格进度条
+    QtMaterialSlider *m_slider1;              // Material设计风格滑块
+    QtMaterialSlider *m_slider2;              // Material设计风格滑块
+    QtMaterialRaisedButton *m_syncButton;     // Material设计风格凸起按钮
+    
+    // 图像帧数据
+    std::vector<cv::Mat> imageStack;          // 处理前图像栈
+    std::vector<cv::Mat> final_images;        // 处理后图像栈
+    int currentFrame;                         // 当前帧索引
+    bool isSyncMode;                          // 同步模式标志
 };
 
 #endif // MAINWINDOW_H
